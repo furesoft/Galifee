45f8814305f7298f07b2df039e84271c28e2f0e3
Chris Anders <filmee24@gmail.com>
2021-01-05 16:21:21 +0100

    Add WebResouceLoader

---
+++ Source/Loaders/WebResourceLoader.cs
@@ -0,0 +1,25 @@
+﻿using Galifree.Core;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Galifree.Loaders
+{
+    public class WebResourceLoader : IResourceLoader
+    {
+        public WebResourceLoader(string uRI)
+        {
+            URI = uRI;
+        }
+
+        public string URI { get; set; }
+
+        public async Task<Stream> GetStream()
+        {
+            var wr = HttpWebRequest.Create(URI);
+
+            return wr.GetResponse().GetResponseStream();
+        }
+    }
+}
