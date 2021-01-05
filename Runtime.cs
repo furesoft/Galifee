9b0f58458408a1308d0480ac791872ca80c25920
Chris Anders <filmee24@gmail.com>
2021-01-05 12:56:38 +0100

    Initial Commit

---
+++ Runtime.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+using Furesoft.Core.CLI;
+
+namespace Galifree
+{
+    public static class Runtime
+    {
+        public static void Run<App>(string[] args)
+            where App : Application, new()
+        {
+            var cli = new CommandlineArguments(args);
+
+            if (!cli.HasOption("silent"))
+            {
+                //run ui mode
+                AppBuilder.Configure<App>()
+                        .UsePlatformDetect()
+                        .LogToDebug()
+                        .StartWithClassicDesktopLifetime(args);
+            }
+            else
+            {
+                // run cmd mode
+            }
+        }
+    }
+}
