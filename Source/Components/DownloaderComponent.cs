41a10954fd38d452062e83a9236e36309c2e0110
Chris Anders <filmee24@gmail.com>
2021-01-05 18:32:20 +0100

    Add Core for DownloaderComponent

---
+++ Source/Components/DownloaderComponent.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Controls;
+using Galifee.Core;
+
+namespace Galifee.Components
+{
+    public class DownloaderComponent : ISetupComponent
+    {
+        public string Filename { get; set; }
+
+        public void AfterInstall(SetupContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AfterUninstall(SetupContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BeforeInstall(SetupContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BeforeUninstall(SetupContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Control GetVisualControl(SetupContext context)
+        {
+            return new UI.DownloaderUIPanel(this);
+        }
+    }
+}
---
+++ Source/UI/DownloaderUIPanel.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Galifee.Components;
+
+namespace Galifee.UI
+{
+    public class DownloaderUIPanel : UserControl
+    {
+        public DownloaderUIPanel(DownloaderComponent downloaderComponent)
+        {
+            this.InitializeComponent();
+            DownloaderComponent = downloaderComponent;
+        }
+
+        public DownloaderComponent DownloaderComponent { get; }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
