c1d723edb1be9151cee1d57bf37c8e210b20062e
Chris Anders <filmee24@gmail.com>
2021-01-05 16:23:14 +0100

    Add IResourceExtractor

---
+++ Source/Core/IResourceExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Galifree.Core
+{
+    public interface IResourceExtractor
+    {
+        /// <summary>
+        /// Extract a Resource Stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns>#path to extracted Data Folder</returns>
+        Task<string> Extract(Stream stream);
+    }
+}
