917c62d0160d4f3189e2a245152f552e341ce99f
Chris Anders <filmee24@gmail.com>
2021-01-05 18:26:54 +0100

    Add FileResourceLoader

---
+++ Source/Loaders/FileResourceLoader.cs
@@ -0,0 +1,16 @@
+﻿using Galifee.Core;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Galifee.Loaders
+{
+    public class FileResourceLoader : IResourceLoader
+    {
+        public string Filename { get; set; }
+
+        public Task<Stream> GetStream()
+        {
+            return Task.FromResult((Stream)File.OpenRead(Filename));
+        }
+    }
+}
