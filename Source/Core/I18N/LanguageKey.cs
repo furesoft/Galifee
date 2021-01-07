8c5d2677adc2dbe847cff24228b8821503518ad3
Chris Anders <filmee24@gmail.com>
2021-01-07 13:17:00 +0100

    Add MarkupExtension for Language

---
+++ Source/Core/I18N/LanguageKey.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Markup.Xaml;
+using System;
+
+namespace Galifee.Core.I18N
+{
+    public class LanguageKey : MarkupExtension
+    {
+        public LanguageKey(string key)
+        {
+            Key = key;
+        }
+
+        public string Key { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Runtime.Context.LanguageManager.GetValue(Key);
+        }
+    }
+}
