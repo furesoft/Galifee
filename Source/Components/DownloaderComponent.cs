e82b51acc0c8f4988215e6761ab677d13e29dd3a
Chris Anders <filmee24@gmail.com>
2021-01-09 15:37:33 +0100

    Cleanup

--- Source/Components/DownloaderComponent.cs
+++ Source/Components/DownloaderComponent.cs
@@ -5,28 +5,13 @@ using System.Threading.Tasks;
 
 namespace Galifee.Components
 {
-    public class DownloaderComponent : ISetupComponent, IVisualComponent
+    public class DownloaderComponent : IVisualComponent
     {
         public string Filename { get; set; }
 
         public Control GetVisualControl(SetupContext context)
         {
             return new UI.DownloaderUIPanel(this);
-        }
-
-        public Task OnInstall(SetupContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task OnUninstall(SetupContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task OnUpgrade(SetupContext context)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
