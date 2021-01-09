48140f19f1d306d3f232fa727c7fb42c0374ba36
Chris Anders <filmee24@gmail.com>
2021-01-09 15:19:56 +0100

    Add Previous Command

---
+++ Source/UI/Commands/PreviousCommand.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Windows.Input;
+
+namespace Galifee.UI.Commands
+{
+    public class PreviousCommand : MarkupExtension, ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            //ToDo: implement condition for next button
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is Carousel car)
+            {
+                car.PageTransition = new PageSlide(TimeSpan.FromMilliseconds(400));
+                car.Previous();
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new PreviousCommand();
+        }
+    }
+}
